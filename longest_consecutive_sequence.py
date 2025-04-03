efd44e9619f5d7484e07342ae719fc010245feea
Sammy Martinez <asmartinez01@wm.edu>
2025-04-02 22:10:51 -0400

    Added comments for longest_consecutive_sequence

--- longest_consecutive_sequence.py
+++ longest_consecutive_sequence.py
@@ -1,5 +1,5 @@
 #longest consecutive sequence
-
+#Runs in O(n)
 class Solution(object):
     def longestConsecutive(self, nums):
         """
@@ -7,12 +7,19 @@ class Solution(object):
         :rtype: int
         """
         longest = 0
+        #Hash the array List[int] into a set
+
         num_set = set(nums)
+        #0,2,3,1
+        #Find an endpoint for this algorithm(it doesnt have anything lower than it)
 
         for num in num_set:
+            #This a simple look-up since hashing, if nothing lower, the endpoint is One
             if num-1 not in num_set:
                 length = 1
+                #Simple look up, keeps maxing the length with compared hashed values 
                 while num + length in num_set:
                     length +=1
+                
                 longest = max(longest,length)
         return longest
